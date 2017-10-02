f17dd660b92c20cdd2541f425d0e7cf84829cd62
Jimmy Sambuo <jimmy.sambuo@capitalone.com>
2017-10-02 12:18:25 -0400

    Add FaceID test button and function

--- TestFaceID/ViewController.swift
+++ TestFaceID/ViewController.swift
@@ -7,8 +7,40 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBAction func buttonTapped(_ sender: Any) {
+        // Code example from https://developer.apple.com/documentation/localauthentication
+        let myContext = LAContext()
+        let myLocalizedReasonString = "FaceID test"
+        
+        var authError: NSError?
+        if #available(iOS 8.0, macOS 10.12.1, *) {
+            if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+                myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString) { [unowned self] success, evaluateError in
+                    DispatchQueue.main.async {
+                        if success {
+                            // User authenticated successfully, take appropriate action
+                            self.displayLabel?.text = "success"
+                        } else {
+                            // User did not authenticate successfully, look at error and take appropriate action
+                            self.displayLabel?.text = "Error: \(evaluateError.debugDescription)"
+                        }
+                    }
+                }
+            } else {
+                // Could not evaluate policy; look at authError and present an appropriate message to user
+                let message = authError?.debugDescription ?? "nil"
+                self.displayLabel?.text = "No biometrics available: \(message)"
+            }
+        } else {
+            // Fallback on earlier versions
+            self.displayLabel?.text = "iOS version too early."
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
